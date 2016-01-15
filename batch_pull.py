404f11fe8447a48ead86c073605d6cd2d4f6771e
Will Wagner <willwagner602@gmail.com>
2016-01-15 13:58:49 -0500

    Added batch file to pull all apps down from their individual repos

---
+++ batch_pull.py
@@ -0,0 +1,6 @@
+from subprocess import call
+
+if __name__ == '__main__':
+	call(['git', 'clone', 'https://github.com/willwagner602/PyJobsDjango.git'])
+	call(['git', 'clone', 'https://github.com/willwagner602/Thu2d.git'])
+	call(['git', 'clone', 'https://github.com/willwagner602/Thud.git'])
