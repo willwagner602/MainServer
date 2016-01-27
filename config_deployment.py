9ef35051d9cab6e2741bf90b11aaf0d78b5b990f
willwagner602 <willwagner602@gmail.com>
2016-01-26 21:10:40 -0500

    - Added some things to the automatic configuration for apache2

--- config_deployment.py
+++ config_deployment.py
@@ -1,4 +1,10 @@
 from subprocess import call
+import os
 
 if __name__ == '__main__':
+
+    base_dir = os.getcwd()
+
     call(['python3', 'manage.py', 'collectstatic'])
+    call(['touch', base_dir + '/Thud/ThudLog.log'])
+    call(['touch', base_dir + '/Thud/games.db'])
