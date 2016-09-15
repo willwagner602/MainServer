e9a134a3e80998fdf7075406990c2efd5a8fd170
Will Wagner <willwagner602@gmail.com>
2016-09-15 14:21:39 -0400

    - fixed top level folder path

--- MainServer/wsgi.py
+++ MainServer/wsgi.py
@@ -13,7 +13,7 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-sys.path.insert(0, os.path.abspath(os.path.abspath(os.path.dirname(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MainServer.settings")
 
