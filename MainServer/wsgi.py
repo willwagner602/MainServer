c72250cd54dea295ecce25d9ed8e29aa7917aab0
Will Wagner <willwagner602@gmail.com>
2016-09-15 14:10:08 -0400

    - Added MainServer module to path

--- MainServer/wsgi.py
+++ MainServer/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MainServer.settings")
